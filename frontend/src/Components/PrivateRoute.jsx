b4c4936a93f27f11f164d9b82e9f5c43e866de6c
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-09-17 20:47:43 +0530

    fixed bugs of routing to login page

--- frontend/src/Components/PrivateRoute.jsx
+++ frontend/src/Components/PrivateRoute.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Navigate, Outlet } from "react-router-dom";
 import { GetUserQuery } from "../api/user";
 
 const ProtectedRoute = () => {
   const data = GetUserQuery();
-  const isAuthenticated = data?.data?.name;
-  // If authorized, return an outlet that will render child elements
-  // If not, return element that will navigate to login page
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  const [user, setuser] = useState(true);
+  useEffect(() => {
+    setuser(data);
+  }),
+    [data.data];
+
+  return user ? <Outlet /> : <Navigate to="/login" />;
 };
 export default ProtectedRoute;
