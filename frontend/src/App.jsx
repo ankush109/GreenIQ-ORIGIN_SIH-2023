5a0924e5c415b0c59c01ba19037ea40c9784b1ac
Ankush Banerjee <banerjeeankush184@gmail.com>
2023-09-21 23:39:12 +0530

    chat bot added and left pannel selection done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,6 +26,7 @@ import Material from "./Components/materials/materials";
 import MaterialSubject from "./Components/materials/MaterialSubject";
 import CreateMaterial from "./Components/mentor/CreateMaterial";
 import Sathi from "./Components/virtual-mentor/Sathi";
+import Chatbot from "./Components/virtual-mentor/chatbot";
 function App() {
   function isJWTValid() {
     const token = localStorage.getItem("token");
@@ -71,28 +72,29 @@ function App() {
             <Route path="/login" element={<Login />} />
           </Route>
 
-          <Route path="/user" element={<Dashboard/>}>
-              <Route path="courses" element={<Courses />} />
-              <Route path="sathi" element={<Sathi />} />
-              <Route path="material/:id" element={<MaterialSubject />} />
-              <Route path="test" element={<Test />} />
-              <Route path="material" element={<Material />} />
-              <Route path="book-meeting" element={<Meeting />} />
-              <Route path="discuss" element={<Discuss />} />
-              <Route path="report" element={<Report />} />
-              <Route path="leaderboard" element={<Leaderboard />} />
-              <Route path="newsfeed" element={<Newsfeed />} />
-              <Route path="profile" element={<Profile />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="confirm-booking/:id" element={<ConfirmBooking />} />
+          <Route path="/user" element={<Dashboard />}>
+            <Route path="chatbot" element={<Chatbot />} />
+            <Route path="courses" element={<Courses />} />
+            <Route path="sathi" element={<Sathi />} />
+            <Route path="material/:id" element={<MaterialSubject />} />
+            <Route path="test" element={<Test />} />
+            <Route path="material" element={<Material />} />
+            <Route path="book-meeting" element={<Meeting />} />
+            <Route path="discuss" element={<Discuss />} />
+            <Route path="report" element={<Report />} />
+            <Route path="leaderboard" element={<Leaderboard />} />
+            <Route path="newsfeed" element={<Newsfeed />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="confirm-booking/:id" element={<ConfirmBooking />} />
           </Route>
-          <Route path="/mentor" element={<Dashboard/>}>
-              <Route path="my-Test" element={<Mentortest />} />
-              <Route path="createtest" element={<CreateTest />} />
-              <Route path="Meetings" element={<Meetings />} />
-              <Route path="createMaterial" element={<CreateMaterial />} />
-              <Route path="material" element={<Material />} />
-              <Route path="material/:id" element={<MaterialSubject />} />
+          <Route path="/mentor" element={<Dashboard />}>
+            <Route path="my-Test" element={<Mentortest />} />
+            <Route path="createtest" element={<CreateTest />} />
+            <Route path="Meetings" element={<Meetings />} />
+            <Route path="createMaterial" element={<CreateMaterial />} />
+            <Route path="material" element={<Material />} />
+            <Route path="material/:id" element={<MaterialSubject />} />
           </Route>
         </Routes>
       </BrowserRouter>
